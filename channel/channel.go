5d57860737b8b5b5df2a953b7a0c8fcad06f8b2f
anubhavpurohit <anubhavpurohitno1@gmail.com>
2024-12-24 23:49:09 +0530

    Learning bufferchannel

---
+++ channel/channel.go
@@ -0,0 +1,27 @@
+package main
+
+// func processNumb(num chan int) {
+// 	fmt.Println("num channel", <-num)
+// }
+
+func main() {
+
+	// learning bufferchannel
+	// buffer channel
+
+	// ////////////////////////////////////////////////////////////////////////////
+	// makechannel := make(chan int)
+
+	// go processNumb(makechannel)
+
+	// makechannel <- 5
+	// time.Sleep(time.Second * 2)
+	// ////////////////////////////////////////////////////////////////////
+	// messagechn := make(chan string)
+	// messagechn <- "ping"
+
+	// msg := <-messagechn
+
+	// reciveing and sending helps to block the program
+
+}
